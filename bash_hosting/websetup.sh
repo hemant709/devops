c9fff8a0bdd5e642d698b70d1c81afde8f433376
root <root@ip-172-31-80-168.ec2.internal>
2024-10-01 06:35:34 +0000

    random web setup

--- bash_hosting/websetup.sh
+++ bash_hosting/websetup.sh
@@ -1,39 +1,46 @@
 #!/bin/bash
 
+#VARIABLES 
+PACKAGES="apache2 unzip wget"
+SVC="apache2"
+TMP_DIR="/tmp/webfiles"
+WEB_URL="https://www.tooplate.com/zip-templates/2098_health.zip"
+ART_NAME="2098_health"
+
 # Installing Dependencies
 echo "########################################"
 echo "Installing packages."
 echo "########################################"
 sudo apt update
-sudo apt install apache2 -y >> /dev/null
+sudo apt install $PACKAGES -y >> /dev/null
 echo
 
 # Start & Enable Service
 echo "########################################"
-echo "Start & Enable HTTPD Service"
+echo "Start & Enable APACHE2 Service"
 echo "########################################"
-sudo systemctl start apache2
-sudo systemctl enable apache2
+sudo systemctl start $SVC
+sudo systemctl enable $SVC
 echo
 
 # Creating Temp Directory
 echo "########################################"
 echo "Starting Artifact Deployment"
 echo "########################################"
-mkdir -p /tmp/webfiles
-cd /tmp/webfiles
+mkdir -p $TMP_DIR
+cd $TMP_DIR
 echo
 
-wget https://www.tooplate.com/zip-templates/2098_health.zip > /dev/null
-unzip 2098_health.zip > /dev/null
-sudo cp -r 2098_health/* /var/www/html/
+wget $WEB_URL > /dev/null
+unzip $ART_NAME.zip > /dev/null
+sudo cp -r $ART_NAME/* /var/www/html/
 echo
 
 # Bounce Service
 echo "########################################"
-echo "Restarting HTTPD service"
+echo "Restarting APACHE2 service"
 echo "########################################"
-systemctl restart apache2
+systemctl restart $SVC
 echo
 
 # Clean Up
@@ -41,8 +48,7 @@ echo "########################################"
 echo "Removing Temporary Files"
 echo "########################################"
 rm -rf /tmp/webfiles
-echo
 
-sudo systemctl status apache2
+sudo systemctl status $SVC
 ls /var/www/html/
 
