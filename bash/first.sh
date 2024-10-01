0ce4b25eb3895455d11eabcd13ee5de1181e19c4
root <root@ip-172-31-80-168.ec2.internal>
2024-10-01 05:26:57 +0000

    random web host

---
+++ bash/first.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Installing Dependencies
+echo "########################################"
+echo "Installing packages."
+echo "########################################"
+sudo apt update
+sudo apt install apache2 -y >> /dev/null
+echo
+
+# Start & Enable Service
+echo "########################################"
+echo "Start & Enable HTTPD Service"
+echo "########################################"
+sudo systemctl start apache2
+sudo systemctl enable apache2
+echo
+
+# Creating Temp Directory
+echo "########################################"
+echo "Starting Artifact Deployment"
+echo "########################################"
+mkdir -p /tmp/webfiles
+cd /tmp/webfiles
+echo
+
+wget https://www.tooplate.com/zip-templates/2098_health.zip > /dev/null
+unzip 2098_health.zip > /dev/null
+sudo cp -r 2098_health/* /var/www/html/
+echo
+
+# Bounce Service
+echo "########################################"
+echo "Restarting HTTPD service"
+echo "########################################"
+systemctl restart apache2
+echo
+
+# Clean Up
+echo "########################################"
+echo "Removing Temporary Files"
+echo "########################################"
+rm -rf /tmp/webfiles
+echo
+
+sudo systemctl status apache2
+ls /var/www/html/
+
